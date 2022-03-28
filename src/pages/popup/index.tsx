f6768bee73995bc898040c29a6778f729ce91e97
rossmoody <fiftyfivehis@gmail.com>
2022-03-27 19:11:45 -0700

    Update index.tsx

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
-console.log('popup script')
-
 const root = document.querySelector('#root')
 
 render(<App />, root)
